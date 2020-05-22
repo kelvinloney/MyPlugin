567dce21310516f679e21703ed1ecfc3e6894982
kelvinloney <kelvin.l.loney@gmail.com>
2020-05-22 16:37:22 -0400

    Add files via upload

---
+++ myplugin.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Plugin Name: MyPlugin
+Description: Customize your login page and admin area.
+Plugin URI:  https://github.com/kelvinloney/MyPlugin
+Author:      Kelvin Loney
+Version:     1.0
+Text Domain: myplugin
+Domain Path: /languages
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+
+// exit if file is called directly
+if ( ! defined( 'ABSPATH' ) ) {
+
+	exit;
+
+}
+
+
+
+// load text domain
+function myplugin_load_textdomain() {
+
+	load_plugin_textdomain( 'myplugin', false, plugin_dir_path( __FILE__ ) . 'languages/' );
+
+}
+add_action( 'plugins_loaded', 'myplugin_load_textdomain' );
+
+
+
+// if admin area
+if ( is_admin() ){
+
+  // include dependencies
+  require_once plugin_dir_path( __FILE__ ) . 'admin/admin-menu.php';
+  require_once plugin_dir_path( __FILE__ ) . 'admin/settings-page.php';
+  require_once plugin_dir_path( __FILE__ ) . 'admin/settings-register.php';
+  require_once plugin_dir_path( __FILE__ ) . 'admin/settings-callbacks.php';
+  require_once plugin_dir_path( __FILE__ ) . 'admin/settings-validate.php';
+
+}
+
+
+
+// include dependencies: admin and public
+require_once plugin_dir_path( __FILE__ ) . 'includes/core-functions.php';
+
+
+
+// default plugin options
+function myplugin_options_default() {
+
+	return array(
+		'custom_url'     => 'https://wordpress.org/',
+		'custom_title'   => esc_html__( 'Powered by WordPress', 'myplugin' ),
+		'custom_style'   => 'disable',
+		'custom_message' => '<p class="custom-message">' . esc_html__( 'My custom message', 'myplugin' ) . '</p>',
+		'custom_footer'  => esc_html__( 'Special message for users', 'myplugin' ),
+		'custom_toolbar' => false,
+		'custom_scheme'  => 'default',
+	);
+
+}
+
+
+
+
+// remove options on uninstall
+function myplugin_on_uninstall() {
+
+	if ( ! current_user_can( 'activate_plugins' ) ) return;
+
+	delete_option( 'myplugin_options' );
+
+}
+register_uninstall_hook( __FILE__, 'myplugin_on_uninstall' );
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Plugin Name ===
+
+Plugin Name: MyPlugin
+Description: Customize your login page and admin area.
+Plugin URI:  https://github.com/kelvinloney/MyPlugin
+Author:      Kelvin Loney
+Version:     1.0
+Text Domain: myplugin
+Domain Path: /languages
+License:     GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+== Description ==
+
+This plugin allows the user to customize their login page and admin area.
+
+The customization options are as follows:
+
+Customize Login Page
+*   Custom URL
+*   Custom Title
+*   Custom Style
+*   Custom Message
+
+Customize Admin Area
+*   Custom Footer
+*   Custom Toolbar
+*   Custom Scheme
+
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload the plugin files to the `/wp-content/plugins/myplugin` directory
+2. Activate the plugin through the 'Plugins' screen in WordPress
+3. Use the Settings->MyPlugin screen to configure the plugin
+
+
+
+== Screenshots ==
+
+Screenshots can be viewed in the "screenshots" folder.
+
+In the folder you will find the plugin's:
+*   index page
+*   login page
+*   settings page
+
+
+
+== Changelog ==
+
+= 1.0 =
+* No changes have been made to this version.
